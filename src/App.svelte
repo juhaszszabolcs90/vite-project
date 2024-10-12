d432926afe1c5e2f6e1bdf4b2fc380abbefce887
juhaszszabolcs90 <juhaszszabolcs90@gmail.com>
2024-10-12 10:56:08 +0200

    initialize svelte project

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  var time = new Date();
+  var szam = 0;
+
+  function update() {
+    
+  }
+  setInterval(() => {
+    time = new Date();
+    1000
+  })
+</script>
+
+<main>
+  <h1>{time.toLocaleTimeString()}</h1>
+  <p>{time.toLocaleString()}</p>
+  
+  {time.toLocaleDateString()}
+
+  <hr><input type="text">
+</main>
+
+<style>
+
+</style>
